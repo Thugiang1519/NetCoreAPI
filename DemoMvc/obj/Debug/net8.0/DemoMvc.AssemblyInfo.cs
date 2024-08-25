7db0e19d066153b03ec772d065a5fb7cee9f9aa4
Thugiang1519 <2121051276@student.humg.edu.vn>
2024-08-25 17:42:17 +0700

    basic

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd23496102fd73ec7eb0e7a14e5543066e64ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454b37b98bf52227ebfda0a13dc8fdba236634fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
